e71cfb1691ebc9f7246964fe6e975bc52c808eba
hbh <sout1217@naver.com>
2021-04-27 13:26:25 +0900

    1-4 단어 뒤집기 (StringBuilder 이용)

--- src/com/company/demo/Main.java
+++ src/com/company/demo/Main.java
@@ -1,73 +1,70 @@
 package com.company.demo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
-* 설명
+ * 4. 단어 뒤집기
+ * 설명
  *
- * 한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
- *
- * 문장속의 각 단어는 공백으로 구분됩니다.
+ * N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
  *
  *
  * 입력
- * 첫 줄에 길이가 100을 넘지 않는 한 개의 문장이 주어집니다. 문장은 영어 알파벳으로만 구성되어 있습니다.
+ * 첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+ *
+ * 두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
  *
  *
  * 출력
- * 첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한
- *
- * 단어를 답으로 합니다.
-* */
+ * N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+ * */
 
 /**
- * 예시답안
- * it is time to study
+ * 예시 입력
+ * 3
+ * good
+ * Time
+ * Big
  *
- * 예시출력
- * study
+ * 예시 출력
+ * doog
+ * emiT
+ * giB
  * */
 
 public class Main {
 
-    public String solution(String str) {
+    public ArrayList<String> solution(int num, String[] strArr) {
 
-        String answer = "";
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        int minValue = Integer.MIN_VALUE;
-        int pos;
+        for (String s : strArr) {
+            String tmp = new StringBuilder(s).reverse().toString();
+            // String + String 하거나, String replaceAll 사용 시 새로운 객체를 사용하여 메모리를 낭비시킨다
+            // StringBuilder 는 처음에 만들어진 객체 하나로만 사용한다
 
-        while((pos = str.indexOf(" ")) != -1) {
-            // pos = str.indexOf(" ") = 2 번째 인덱스를 반환한다
-            // 그리고 while 로 -1 반환 (존재하지 않을 때 까지) 반복한다
-
-            String tmp = str.substring(0, pos);
-            // 처음 가져온 단어를 넣는다 (subString(0, 2) 을 통해 0~1 까지 단어를 자른다
-
-            int len = tmp.length();
-            // 해당 단어의 문자길이를 반환
-
-            if (len > minValue) {
-                minValue = len;
-                answer = tmp;
-            }
-            // 가장 작은 값보다 큰 경우 값 변경
-
-            str = str.substring(pos + 1);
-            // it is time to study.substring(3) 부터 잘라내어
-            // is time to study 문장이 된다다
+            arrayList.add(tmp);
         }
-        if (str.length() > minValue) answer = str;
 
-        return answer;
+        return arrayList;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(T.solution(str));
 
+        int num = sc.nextInt(); // 첫 번째 숫자를 읽어드림
+
+        String[] strArr = new String[num]; // 입력한 String 이 담길 배열 미리 생성
+
+        for (int i = 0; i < strArr.length; i++) { // String 배열 길이만큼 입력값 받기
+            strArr[i] = sc.next();
+        }
+
+        for (String s : T.solution(num, strArr)) { // solution 에서 반환되는 list 를 출력 (배열을 돌면서 문자열을 뒤집음)
+            System.out.println(s);
+        }
     }
 }
